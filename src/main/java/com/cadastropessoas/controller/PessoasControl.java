76ce4ad974ad083b2392adae2dbf610b3d752040
4andrewtanakashi <andrew.vita@estudante.ufla.br>
2019-09-28 18:58:50 -0300

    :rocket: Test III

--- src/main/java/com/cadastropessoas/controller/PessoasControl.java
+++ src/main/java/com/cadastropessoas/controller/PessoasControl.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +21,7 @@ import com.cadastropessoas.models.Pessoa;
 //import com.cadastropessoas.repository.PessoaRepository;
 import com.cadastropessoas.service.PessoasService;
 
-//@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:4200", "https://agile-refuge-51650.herokuapp.com"})
 @RestController
 public class PessoasControl {
 	
